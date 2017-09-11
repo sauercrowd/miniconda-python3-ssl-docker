2a41a6bddfbd109362996d29f7afc02fec158ac2
Jonas <sauercrowd@users.noreply.github.com>
2017-09-11 22:24:28 +0200

    fixed path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 openssl req -x509 -nodes -days 365 -subj "/C=DE/ST=Sauercrowd/L=Sauercrowd/O=Sauercrowd/OU=IT Department/CN=example.com" -newkey rsa:2048 -keyout /home/conda/ssl/jupyter.key -out /home/conda/ssl/jupyter.crt
-/home/conda/anaconda3/bin/jupyter notebook --certfile=/home/conda/ssl/jupyter.crt --keyfile=/home/conda/ssl/jupyter.key --ip=0.0.0.0 --no-browser
+/home/conda/miniconda3/bin/jupyter notebook --certfile=/home/conda/ssl/jupyter.crt --keyfile=/home/conda/ssl/jupyter.key --ip=0.0.0.0 --no-browser
